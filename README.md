e4d34793c8d78a11cb980f236230905b1723ef1e
Vini <blekshotvgzin@gmail.com>
2025-07-29 18:17:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<<<<<<< HEAD
+# VSUVHSBBE
+=======
+"# ChatT" 
+"# MeuCHat" 
+"# MeuCHat" 
+"# MeuCHat" 
+"# MeuCHat" 
+"# MeuCHat" 
+"# CHATTT" 
+"# VSUVHSBBE" 
+"# VSUVHSBBE" 
+>>>>>>> c324e03 (first commit)
+"# VSUVHSBBE" 
+"# CHHAT" 
+<<<<<<< HEAD
+=======
+"# CHHAT" 
+>>>>>>> 286b66b (first commit)
+"# CHHAT" 
+"# MeuCHat" 
+"# ch" 
+"# ch" 
+"# ch" 
+"# chattttttt" 
---
+++ server.js
@@ -0,0 +1,43 @@
+const WebSocket = require('ws');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+// Servidor HTTP para servir o index.html
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+  let ext = path.extname(filePath);
+
+  let contentType = 'text/html';
+  if (ext === '.js') contentType = 'text/javascript';
+  if (ext === '.css') contentType = 'text/css';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
+});
+
+// Servidor WebSocket
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', ws => {
+  ws.on('message', msg => {
+    // Enviar a mensagem para todos os clientes conectados, incluindo o remetente
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(msg.toString());
+      }
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
